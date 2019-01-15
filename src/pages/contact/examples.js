3458e323fa2de4dc0710f058d537f8d1ab557b92
Ion <19718355+Nei-V@users.noreply.github.com>
2019-01-15 23:36:58 +0200

    cleaned portofolio

--- src/pages/contact/examples.js
+++ src/pages/contact/examples.js
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "gatsby-link";
 import Layout from '../../components/Layout'
 
+
 export default class Index extends React.Component {
   render() {
     return (
